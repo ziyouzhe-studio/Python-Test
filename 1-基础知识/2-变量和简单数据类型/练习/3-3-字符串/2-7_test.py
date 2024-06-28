d9955094c880888f700f9bb9a81b3dc5179fba5c
ZiYouZhe <ZiYouZhe@git.com>
2024-06-29 00:12:19 +0800

    用变量表示一个人的名字，名字左右都空格，使用方法去除空格，打印输出

---
+++ 1-基础知识/2-变量和简单数据类型/练习/3-3-字符串/2-7_test.py
@@ -0,0 +1,10 @@
+# 提出空白，用变量表示一个人的名字，在其开头和结尾都包含一些空白。至少使用"\t"、"\n"各一次
+# 然后分别使用lstrip()、rstrip()、strip()进行处理
+# strip()删除两端空白
+# lstrip()删除左侧空白
+# rstrip()删除右侧空白
+name = " tom "
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())

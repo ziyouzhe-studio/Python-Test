c245ec2d123b9e421759ea34f836f3c4414fd25a
ZiYouZhe <ZiYouZhe@git.com>
2024-06-29 14:46:20 +0800

    改变了目录顺序

---
+++ 1-基础知识/2-变量和简单数据类型/练习/2-3-字符串/2-5_test.py
@@ -0,0 +1,7 @@
+# 名言；将一个名人的名字和名言打印出来
+# 输出内容如下：Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+name = "albert Einstein"
+word = "a person who never made a mistake never tried anything new."
+name_word = f"{name.title()} once said, \"{word.title()}\""
+print(name_word)

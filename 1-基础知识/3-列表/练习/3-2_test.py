610e141f41834bc1275d690b696230593ee4c57a
ZiYouZhe <ZiYouZhe@git.com>
2024-06-30 00:04:56 +0800

    打印姓名列表，附加问候语

---
+++ 1-基础知识/3-列表/练习/3-2_test.py
@@ -0,0 +1,10 @@
+# 问候语
+names = ['tim', 'cook', 'john', 'stephen', 'jim', 'benjamin']
+test1 = f"{names[0].title()}, hello welcome to python\n"
+test2 = f"{names[1].title()}, hello welcome to python\n"
+test3 = f"{names[2].title()}, hello welcome to python\n"
+test4 = f"{names[3].title()}, hello welcome to python\n"
+test5 = f"{names[4].title()}, hello welcome to python\n"
+test6 = f"{names[5].title()}, hello welcome to python"
+
+print(test1, test2, test3, test4, test5, test6)

5a2f82be05653e2ca59143173a1ecb6f817ae551
ZiYouZhe <ZiYouZhe@git.com>
2024-06-29 14:48:29 +0800

    删除了一个目录

--- 1-基础知识/2-变量和简单数据类型/练习/3-3-字符串/2-3_test.py
+++
@@ -1,3 +0,0 @@
-# 个性化消息
-hello_name = "Hello Eric,would you like to learn some Python today"
-print(hello_name)
--- 1-基础知识/2-变量和简单数据类型/练习/3-3-字符串/2-4_test.py
+++
@@ -1,5 +0,0 @@
-# 调整名字的大小写
-name = "jim jarmusch"
-print(name)
-print(name.title())
-print(name.upper())

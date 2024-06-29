38d574a497fd679ee54955831b384bd89beef930
ZiYouZhe <ZiYouZhe@git.com>
2024-06-30 00:05:44 +0800

    选择喜欢的交通工具，打印一条宣传语

---
+++ 1-基础知识/3-列表/练习/3-3_test.py
@@ -0,0 +1,8 @@
+# 选择喜欢的交通方式列表，根据列表打印出一些语句
+traffic = ['motocycle','car','bus']
+message1 = "I like " + traffic[0]
+print(message1)
+message2 = "I don't like " + traffic[2]
+print(message2)
+message3 = "I would like " + traffic[1]
+print(message3)
